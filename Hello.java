98acb568780676fc5f8355bd947c66fe7ffb9f79
lecd2 <69981331+lecd2@users.noreply.github.com>
2020-08-20 12:40:43 -0400

    Add files via upload
    Added Hello.java into repository

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*********************************************
+ * Hello Java Program 
+ *********************************************
+ * Cathy Le
+ * August 20, 2020
+ * CMSC 255 - 001
+ ********************************************/
+
+ public class Hello {
+ 	public static void main(String[] args) {
+
+ 		System.out.println("Cathy");
+ 		System.out.println("Hello Java Program");
+ 		System.out.println("Print out name and class information");
+ 		System.out.println("August 20, 2020");
+ 		System.out.println("CMSC 255 - 001");
+ 		System.out.println();
+ 	}
+ }
